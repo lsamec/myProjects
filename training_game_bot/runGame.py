e816b6c25e80f51215445ca08c4014528065a7ba
Unknown <leon.samec@gmail.com>
2018-05-16 01:49:43 +0200

    minor change

--- training_game_bot/runGame.py
+++ training_game_bot/runGame.py
@@ -1,6 +1,5 @@
 import game
 from keras.models import load_model
-from trainingWithNEAT import loadNEATmodel
 
 board = game.Board(5, 5)
 board.setPlayers(game.AITrainingPlayer("1",board,load_model('models/temp/modelJustFinishedMC')),game.HumanPlayer("2"))
